fc2711192c1eef5afb85d0201f385b1472bd7c00
Tìtstewan <titstewan@learnnavi.org>
2017-02-18 22:24:21 +0100

    Added German translation :D

---
+++ Modification.german-utf8.php
@@ -0,0 +1,92 @@
+<?php
+// Enhanced Help mod
+// Main stuff
+$txt['ln_main_title'] = 'LearnNavi Community Allgemeine Hilfe und Information';
+$txt['ln_bbc_help'] = 'Bulletin Board Code (BBC) Hilfe';
+$txt['ln_bbc_main'] = 'Bulletin Board Codes oder BBCodes sind eine Möglichkeit Beiträge in SMF und vielen anderen Orten Online. Einige Foren haben besondere BBCodes für ihr Bedürfnis durch verschiedene Modifikationen. Weil diese Codes eine Eigenkreation sind, sind diese nicht in der offiziellen Hilfeseite oder im SMF Online-Handbuch verfügbar.';
+$txt['ln_bbc_main2'] = 'Hier findest du alle BBCodes, die du auf LearnNavi.org Forum verwenden kannst:';
+$txt['ln_bbc_syn'] = 'Aufbau';
+$txt['ln_bbc_exa'] = 'Beispiel';
+$txt['ln_bbc_out'] = 'Resultat';
+$txt['ln_bbc_tex'] = 'Text';
+$txt['ln_bbc_opt'] = 'Option';
+$txt['ln_bbc_lin'] = 'Link';
+$txt['ln_group_page'] = 'Nützliche Links: <a id="button_rss" class="new_win" href="http://forum.learnnavi.org/index.php?topic=6770.0">Website-Regeln</a> | <a id="button_rss" class="new_win" href="http://forum.learnnavi.org/index.php?action=groups">Gruppenseite</a>';
+// General help
+$txt['ln_main_text1'] = '<strong>Einloggen</strong><br />Um erfolgreich einzuloggen, musst du dein <strong>Benutzernamen</strong> (nicht zu verwechseln mit dein <strong>Anzeigenamen</strong>!) benutzen. Ein Benutzername ist der Name zum Einloggen in ein SMF Forum und es ist der Name, welches du beim registrieren deines Benutzerkonto für dieses Forum benutzt hast.';
+$txt['ln_main_text2'] = '<strong>Sicherheit des Benutzerkontos</strong><br />Du solltest dein Benutzerkonto für das Forum Aufmerksamkeit schenken, insbesondere den Schutz. Logge dich aus dem Forum, wenn du gehen musst oder du nicht am Computer bist, um Kontodiebstal zu vorzubeugen.';
+// Maybe some more text for the future
+// Examples
+$txt['ln_bbc_b'] = 'Dieser Text ist fett.';
+$txt['ln_bbc_i'] = 'Dieser Text ist kursiv.';
+$txt['ln_bbc_u'] = 'Dieser Text ist unterstrichen.';
+$txt['ln_bbc_s'] = 'Dieser Text ist durchgestrichen';
+$txt['ln_bbc_ean'] = 'Dieser Text ist blau.';
+$txt['ln_bbc_big'] = 'Dieser Text wird groß.';
+$txt['ln_bbc_font'] = 'Dieser Text benutzt die Courier-Schrift';
+$txt['ln_bbc_font_info'] = 'Nur Schriften, die auf den Computer des Benutzers installiert sind, werden angezeigt, daher benutzte es mit Vorsicht!';
+$txt['ln_bbc_left'] = 'Dieser Text ist links.';
+$txt['ln_bbc_center'] = 'Dieser Text ist zentriert.';
+$txt['ln_bbc_right'] = 'Dieser Text ist rechts';
+$txt['ln_bbc_ln_link'] = 'http://learnnavi.org/';
+$txt['ln_bbc_ln_text'] = 'LearnNavi.org';
+$txt['ln_bbc_iurl'] = 'Der iurl-Tag verlinkt zu ein Anker oder ein bestimmte ID (wie Beitrags-ID) im selben Fenster oder Browsertab. Dieser Tag kann auch benutzt werden, um andere Seiten zu verlinken - eine normale url - aber es öffnet kein neues Fenster oder Tab.
+Jene ist besonders nützlich für große Beiträge, welche über ein Inhaltsverzeichnis verfügen.';
+$txt['ln_bbc_lin2'] = 'Link, #Ankername oder #ID';
+$txt['ln_bbc_anchor'] = 'Ein einzigartiger Name, Buchstaben-Zahlenkombination, oder Zahlen.';
+$txt['ln_bbc_anchor2'] = 'Text oder lesse es leer';
+$txt['ln_bbc_email'] = 'Email';
+$txt['ln_bbc_email_text'] = 'Schreibe mir eine E-Mail';
+$txt['ln_bbc_table1'] = 'definiert eine Tabelle';
+$txt['ln_bbc_table2'] = 'definiert eine Zeile';
+$txt['ln_bbc_table3'] = 'definiert eine Zelle';
+$txt['ln_bbc_table4'] = 'Man muss alle Tabellen-Tags zusammen benutzen wie im folgenden Beispiel:';
+$txt['ln_bbc_spoiler'] = 'Dieser Test ist versteckt.';
+$txt['ln_bbc_desc'] = 'Text oder Übersetzung.';
+$txt['ln_bbc_desc1'] = 'Kaltxì!';
+$txt['ln_bbc_desc2'] = 'Hallo!';
+$txt['ln_bbc_col_spec'] = 'Es gibt vier spezielle Farb-Tags:';
+$txt['ln_bbc_trans'] = 'Dieser Text sollte durchsichtig sein.';
+$txt['ln_bbc_li_1'] = 'Das "list" Element kann folgendes als Option haben';
+$txt['ln_bbc_li_2'] = 'Folgende Optionen können durch Ersetzung des ersten "li"-Elements benutzt werden: ';
+$txt['ln_bbc_img'] = 'Link zum Bild';
+$txt['ln_bbc_img_opt'] = 'Der img-Tag kann bis zu 3 optionale Attribute haben. X und Y sollte eine Zahl sein während "alt" ein Text haben kann, das sichtbar wird sobald das Bild nicht geladen werden konnte.';
+$txt['ln_bbc_sup'] = 'Dieser Text ist hochgestellt';
+$txt['ln_bbc_sub'] = 'Dieser Text ist tiefgestellt';
+$txt['ln_bbc_glow'] = 'Die drei Attribute (z.B. red, 2, 50) im glow-Tag ist für Farbe, Stärke und Weite.';
+$txt['ln_bbc_shadow'] = 'Die zwei Attribute (z.B. red, left) im shadow-Tag ist für die Farbe und Richtung.';
+$txt['ln_bbc_rtl1'] = 'Rechts-zu-Links!';
+$txt['ln_bbc_ltr2'] = 'Links-zu-Rechts!';
+$txt['ln_bbc_rtl'] = 'Typische Benutzung wäre das Posten in Arabisch oder Hebräisch in einem englischen Forum.';
+$txt['ln_bbc_ltr'] = 'Typische Benutzung wäre das Posten in Englisch in einem arabischen oder hebräischen Forum.';
+$txt['ln_bbc_ac_ab'] = 'Zeigt den vollen Ausdruck für die Abkürzung oder den Akronym sobald die Maus über den Text ist.';
+$txt['ln_bbc_abb'] = 'Abkürzung';
+$txt['ln_bbc_acr'] = 'Akronym';
+$txt['ln_bbc_qauth'] = 'Autor';
+$txt['ln_bbc_qtext'] = 'Zitierter Beitrag';
+$txt['ln_bbc_audio'] = 'Link zu einer Audiodatei, das eine .mp3, .ogg oder .wav Datei sein sollte.';
+$txt['ln_bbc_nobbc'] = 'Alle BB Codes und Links werden innerhalb dieses Tags deaktiviert.';
+$txt['ln_bbc_time'] = 'Konvetiert ein Unix-Zeitstempel in eine für Menschen lesbare Form.';
+// Titles
+$txt['ln_bbc_1'] = 'Fett / Kursiv / Unterstreichen / Durchstreichen';
+$txt['ln_bbc_2'] = 'Farbe';
+$txt['ln_bbc_3'] = 'Schriftgröße';
+$txt['ln_bbc_4'] = 'Schriftart';
+$txt['ln_bbc_5'] = 'Links / Zentriert / Rechts';
+$txt['ln_bbc_6'] = 'Hyperlink und Anker';
+$txt['ln_bbc_7'] = 'E-Mail';
+$txt['ln_bbc_8'] = 'Tabelle';
+$txt['ln_bbc_9'] = 'Spoiler';
+$txt['ln_bbc_10'] = 'Beschreibung';
+$txt['ln_bbc_11'] = 'Liste';
+$txt['ln_bbc_12'] = 'Bilder einbetten';
+$txt['ln_bbc_13'] = 'Zitat';
+$txt['ln_bbc_14'] = 'Code';
+$txt['ln_bbc_15'] = 'PHP Code';
+$txt['ln_bbc_16'] = 'Hochstellen und Tiefstellen';
+$txt['ln_bbc_17'] = 'Glühen und Schatten';
+$txt['ln_bbc_18'] = 'Rechts-zu-Links und Links-zu-Rechts';
+$txt['ln_bbc_19'] = 'Abkürzung und Akronym';
+$txt['ln_bbc_20'] = 'Mehr BB Codes (nur Syntax)';
+// end of Enhanced Help mod
+?>
